82b093e728e03bd8d8088419bedb78e59e62c112
Chaos <weghofer.sam@gmail.com>
2023-03-08 08:10:49 +0100

    Adding files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Portfolio for [my Website](https://www.almightychaos.dev)
---
+++ src/script.js
@@ -0,0 +1,14 @@
+/* -- Glow Effect -- */
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 1500, fill: "forwards" });
+}
+
+/* -- Other -- */
